5aea82b2a2c0341fc9c55230f695ecaa0c1c8764
Ali Baker <alivxlive@gmail.com>
2020-12-27 00:11:43 +0200

    update setup.py 🚀 and .gitignore

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ f = open("README.md", "r")
 LONG_DESCRIPTION = f.read()
 f.close()
 
+requirements = (
+    ["cement==3.0.4", "redis==3.5.3", "redis-py-cluster==2.1.0", "jinja2", "colorlog"],
+)
 setup(
     name="redismirror",
     version=VERSION,
     description="Mirror Redis Traffic to another redis node",
-    long_description="Mirror Redis Traffic to another redis node",
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     author="Ali Saleh Baker",
     author_email="alivxlive@gmail.com",
