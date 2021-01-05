7d21ec722f692f805ce4a8af88cd4bfc9aa1581e
Ali Baker <alivxlive@gmail.com>
2021-01-05 21:06:55 +0200

    ✨ add tcounter option to the script

--- redismirror/main.py
+++ redismirror/main.py
@@ -1,25 +1,26 @@
 import click
 import sys
 import redis
+from time import sleep
 import re
 from redisCommand import *
 
 # TO DO:
 # 1. Add timer to mirror process
-# 2. Add auth option
 # 3. Support to split it to multi node
 # 4. Support all redis command
 # 5. Add number of item to mirror limit
 
 
 @click.command()
-@click.option("--host", default="127.0.0.1", help="Destination redis host/IP.")
-@click.option("--port", default=6379, help="Destination redis port.")
-@click.option("--db", default="0", required=False, help="Destination redis DB.")
-@click.option("--auth", required=False, help="Destination redis auth info.")
-def main(host, port, db, auth):
+@click.option("--host", type=str, default="127.0.0.1", help="Destination redis host/IP.")
+@click.option("--port", type=int, default=6379, help="Destination redis port.")
+@click.option("--db", type=int, default="0", required=False, help="Destination redis DB.")
+@click.option("--auth", type=str, required=False, help="Destination redis auth info.")
+@click.option("--timer", type=int, required=False, help="add running timer in seconds.")
+def main(host, port, db, auth, timer):
     r = makeConnection(host, port, db, auth)
-    getSTDOUT(r)
+    getSTDOUT(r,timer)
 
 
 def makeConnection(host, port, db, auth):
@@ -51,24 +52,31 @@ def makeConnection(host, port, db, auth):
     return r
 
 
-def getSTDOUT(connection):
+def getSTDOUT(connection,timer):
     if not sys.stdin.isatty():
         input_stream = sys.stdin
     else:
         print("There is no stdin, check help for more info. exit 1")
         sys.exit(1)
+    timerCounter = 0
     for line in input_stream:
         try:
-            tmpLine = line.split(" ", 5)
-            keyType = str(tmpLine[3]).strip('"')
-            if keyType == "set":
-                setFunc(line, connection)
-            elif keyType == "hset":
-                hsetFunc(line, connection)
+            timerCounter = timerCounter + 1
+            sleep(1)
+            if not timerCounter == timer:
+                tmpLine = line.split(" ", 5)
+                keyType = str(tmpLine[3]).strip('"')
+                if keyType == "set":
+                    setFunc(line, connection)
+                elif keyType == "hset":
+                    hsetFunc(line, connection)
+                else:
+                    print(f"key type ({keyType}) if not supported yet.")
             else:
-                print(f"key type ({keyType}) if not supported yet.")
-        except:
-            print("Skip line | {0} ....".format(line[:100]))
+                print(f"Finished at second {timerCounter}")
+                sys.exit(0)
+        except Exception as e:
+            print(f"Skip line({line[:30]})")
 
 
 if __name__ == "__main__":
